5a32e12be07015dfad5c53970326c70b1ae2790d
manugraff <manugrafff@gmail.com>
2025-11-02 15:57:41 -0300

    Brand+Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { Route, Routes } from "react-router-dom"
 import { CategoryLayout } from "./cases/categories/components/category-layout"
+import { CategoryForm } from "./cases/categories/components/category-form"
 
 function App() {
-
   return (
     <div className="wrapper">
-      
+        <main>
+            <Routes>
+                <Route path="/categories" element={<CategoryLayout />} >
+                  <Route path="new" element={<CategoryForm />} />
+                  <Route path=":id" element={<CategoryForm />} />
+                </Route>
+            </Routes>
+        </main>
 
-    <main>
-      <Routes>
-        <Route path="/categories" element={ <CategoryLayout/>}>
-          <Route path="new" element={ <CategoryLayout/>}/>
-          <Route path=":id" element={ <CategoryLayout/>}/>
-        </Route>
-      </Routes>
-    </main>
 
     </div>
   )
